fcdd356b30169024a766ca1fb2bc8093676daa8a
Zefanja Timorason <z.timorason@gmail.com>
2023-03-30 16:34:59 +0200

    fixed minour cloudinary bugs

--- server-project/config/cloudinary.config.js
+++ server-project/config/cloudinary.config.js
@@ -5,13 +5,13 @@ const multer = require ('multer')
 cloudinary.config({
    cloud_name: process.env.CLOUDINARY_NAME, 
    api_key: process.env.CLOUDINARY_KEY,
-   api_secret: process.env.CLOUDINARY_API,
+   api_secret: process.env.CLOUDINARY_SECRET,
 })
 
 const storage = new CloudinaryStorage({
    cloudinary,
    params:{
-      allowed_formats: ['jpg', 'png'],
+      allowed_formats: ['jpg', 'png','jpeg'],
       folder: 'project-3'
  }
 })
